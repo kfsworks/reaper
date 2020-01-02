7bbc611a8b5c507fb8e2f35d8d2222838cbfdde4
Sun, Ka Fai <ka_fai_sun@manulifeam.com>
2020-01-02 16:54:12 +0800

    S

---
+++ include/usb_device.hpp
@@ -0,0 +1,12 @@
+#ifndef USB_DEVICE_H_
+#define USB_DEVICE_H_
+
+#include <libusb.h>
+
+class USBDevice {
+  public:
+    USBDevice();
+    ~USBDevice();
+};
+
+#endif // USB_DEVICE_H_
---
+++ src/usb_device.cpp
@@ -0,0 +1,7 @@
+#include "usb_device.hpp"
+
+USBDevice::USBDevice() {
+}
+
+USBDevice::~USBDevice() {
+}
