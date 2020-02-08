b0711ec632a79c1633d9c71f2cadcc91f8263ffb
kfsworks <kfsworks@gmail.com>
2020-02-09 02:33:21 +0800

    S

--- gen-make.sh
+++ gen-make.sh
@@ -1,3 +1,4 @@
-./env-prepare.sh
+# ./env-prepare.sh
+rm -rf ./build/ && mkdir -p build/default include src
 
 export CC=/usr/bin/clang && export CXX=/usr/bin/clang++ && cd build/default  && cmake -D_CMAKE_TOOLCHAIN_PREFIX=llvm- -GNinja ../..
